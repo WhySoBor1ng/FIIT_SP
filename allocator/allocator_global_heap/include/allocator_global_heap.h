24f9fa2fcb5740206c2d1b4e03f8ae0482d181c5
Ivan <vip591313@gmail.com>
2026-05-07 10:51:09 +0300

    fix

--- allocator/allocator_global_heap/include/allocator_global_heap.h
+++ allocator/allocator_global_heap/include/allocator_global_heap.h
@@ -4,44 +4,40 @@
 #include <allocator_dbg_helper.h>
 #include <pp_allocator.h>
 
-class allocator_global_heap final:
-    private allocator_dbg_helper,
-    public smart_mem_resource
+class allocator_global_heap final : private allocator_dbg_helper,
+                                    public smart_mem_resource
 {
 
 private:
-    std::mutex mutex;
+    std::mutex mtx;
 
     static constexpr const size_t size_t_size = sizeof(size_t);
 
 public:
-    
     explicit allocator_global_heap() = default;
-    
+
     ~allocator_global_heap() override = default;
-    
+
     allocator_global_heap(
         allocator_global_heap const &other) = delete;
-    
+
     allocator_global_heap &operator=(
         allocator_global_heap const &other) = delete;
-    
+
     allocator_global_heap(
         allocator_global_heap &&other) noexcept = delete;
-    
+
     allocator_global_heap &operator=(
         allocator_global_heap &&other) noexcept = delete;
 
 private:
-    
     [[nodiscard]] void *do_allocate_sm(
         size_t size) override;
-    
+
     void do_deallocate_sm(
         void *at) override;
 
-    bool do_is_equal(const std::pmr::memory_resource& other) const noexcept override;
-
+    bool do_is_equal(const std::pmr::memory_resource &other) const noexcept override;
 };
 
-#endif //MATH_PRACTICE_AND_OPERATING_SYSTEMS_ALLOCATOR_ALLOCATOR_GLOBAL_HEAP_H
+#endif // MATH_PRACTICE_AND_OPERATING_SYSTEMS_ALLOCATOR_ALLOCATOR_GLOBAL_HEAP_H
--- allocator/allocator_global_heap/src/allocator_global_heap.cpp
+++ allocator/allocator_global_heap/src/allocator_global_heap.cpp
@@ -2,37 +2,19 @@
 #include "../include/allocator_global_heap.h"
 #include <mutex>
 
-[[nodiscard]] void *allocator_global_heap::do_allocate_sm(
-    size_t size)
+[[nodiscard]] void *allocator_global_heap::do_allocate_sm(size_t size)
 {
-    std::lock_guard lock(mutex);
-
-    if (size == 0)
-    {
-        size = 1;
-    }
-
-    return ::operator new(size);
+    std::lock_guard lock(mtx);
+    return operator new(size);
 }
 
-void allocator_global_heap::do_deallocate_sm(
-    void *at)
+void allocator_global_heap::do_deallocate_sm(void *at)
 {
-    std::lock_guard lock(mutex);
-
-    ::operator delete(at);
+    std::lock_guard lock(mtx);
+    operator delete(at);
 }
 
 bool allocator_global_heap::do_is_equal(const std::pmr::memory_resource &other) const noexcept
 {
-    if (this == &other)
-    {
-        return true;
-    }
-    auto other_ptr = dynamic_cast<const allocator_global_heap *>(&other);
-    if (other_ptr == nullptr)
-    {
-        return false;
-    }
-    return other_ptr == this;
+    return this == &other;
 }
